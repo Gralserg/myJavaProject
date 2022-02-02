d87ccaeab3d87285143979f612ca17e0759f9fc9
Gralserg <gralserg@mail.ru>
2022-02-02 20:44:37 +0300

    task3

---
+++ javaLesson1/src/main/java/HomeWork3.java
@@ -0,0 +1,17 @@
+public class HomeWork3 {
+    public static void main(String[] args) {
+        checkSumSign();
+    }
+
+    static void checkSumSign() {
+        int a = 2;
+        int b = -5;
+        if (a + b >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+}
+
+
