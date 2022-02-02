0b06f8cc41b051e4415f5ae0156460cd0053fd60
Gralserg <gralserg@mail.ru>
2022-02-02 21:29:42 +0300

    task5

--- javaLesson1/src/main/java/school/lesson1/HomeWork3.java
+++ javaLesson1/src/main/java/school/lesson1/HomeWork3.java
@@ -1,3 +1,5 @@
+package school.lesson1;
+
 public class HomeWork3 {
     public static void main(String[] args) {
         checkSumSign();
--- javaLesson1/src/main/java/school/lesson1/HomeWorkApp.java
+++ javaLesson1/src/main/java/school/lesson1/HomeWorkApp.java
@@ -4,8 +4,9 @@ public class HomeWorkApp {
     public static void main(String[] args) {
         printTreeWords();
     }
+
     static void printTreeWords() {
         System.out.println("Orange\nBanana\nApple");
     }
-
 }
+
