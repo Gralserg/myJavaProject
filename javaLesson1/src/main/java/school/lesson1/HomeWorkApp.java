e6420d1f6ebc2344c75aaf1f0cd6c3bb0a926e33
Gralserg <gralserg@mail.ru>
2022-02-02 20:11:26 +0300

    task2

--- javaLesson1/src/main/java/school/lesson1/HomeWorkApp.java
+++ javaLesson1/src/main/java/school/lesson1/HomeWorkApp.java
@@ -1,4 +1,11 @@
 package school.lesson1;
 
 public class HomeWorkApp {
+    public static void main(String[] args) {
+        printTreeWords();
+    }
+    static void printTreeWords() {
+        System.out.println("Orange\nBanana\nApple");
+    }
+
 }
