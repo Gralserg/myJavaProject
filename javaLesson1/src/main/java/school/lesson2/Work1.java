c9bde5e058616cf3054d3ed0ec05e25882dc74e9
Gralserg <gralserg@mail.ru>
2022-02-06 18:40:36 +0300

    HomeWork2

---
+++ javaLesson1/src/main/java/school/lesson2/Work1.java
@@ -0,0 +1,15 @@
+package school.lesson2;
+
+public class Work1 {
+
+    static boolean summaTrue(int a, int b) {
+
+
+        return (a + b >= 10 && a + b <= 20);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(summaTrue(5, 10));
+    }
+}
